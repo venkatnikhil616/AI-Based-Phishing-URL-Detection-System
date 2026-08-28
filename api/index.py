39e550852f83f5cfc41792814bf269db9fac8d9e
Venkat Nikhil Vallamsetty <venkatnikhil616@gmail.com>
2026-08-28 22:27:09 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,14 +1,11 @@
 import os
 import sys
 
-# Project root
 BASE_DIR = os.path.abspath(
     os.path.join(os.path.dirname(__file__), "..")
 )
 
-# Make project root available for imports
 if BASE_DIR not in sys.path:
     sys.path.insert(0, BASE_DIR)
 
-# Import Flask application
 from app.app import app
